d613f25fa283ff33f50dcf2c1dc8d9457665d69a
Suvojit Adak <128408901+SuvojitAdak01@users.noreply.github.com>
2024-08-07 16:17:37 +0530

    Create Q2.sql

---
+++ Week 2/PL or SQL Programming/Control Structures/Q2.sql
@@ -0,0 +1,25 @@
+Writing a PL/SQL block that will iterate through all customers and will set a flag IsVIP to TRUE for those with a balance over $10,000.
+
+DECLARE
+    CURSOR cust_cursor IS
+        SELECT customer_id, balance
+        FROM customers
+        WHERE balance > 10000;
+        
+    v_customer_id customers.customer_id%TYPE;
+BEGIN
+    OPEN cust_cursor;
+    
+    LOOP
+        FETCH cust_cursor INTO v_customer_id;
+        EXIT WHEN cust_cursor%NOTFOUND;
+        
+        UPDATE customers
+        SET IsVIP = TRUE
+        WHERE customer_id = v_customer_id;
+    END LOOP;
+    
+    CLOSE cust_cursor;
+    COMMIT;
+END;
+/
